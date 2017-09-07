7bc91469eb2768a782983a7484c5c42e04b5865b
slam24 <luismartinez242011@gmail.com>
2017-09-07 04:35:14 -0600

    feat: Queries

---
+++ graphql/queries/user/multiple.js
@@ -0,0 +1,16 @@
+import {
+	GraphQLList
+} from 'graphql';
+import { userType } from '../../types';
+import UserModel from '../../../models/user';
+
+export default {
+	type: new GraphQLList(userType),
+	resolve(){
+		const users = UserModel.find.exec();
+		if (!users) {
+			throw new Error('Error while fetching users...');
+		}
+		return users;
+	}
+};
